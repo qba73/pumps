64837df512b8eb1e56893b3c3c88c858db6b0a19
Jakub Jarosz <jakub@jarosz.dev>
2023-06-01 09:12:25 +0100

    Add test data

--- pumping.go
+++ pumping.go
@@ -1,1 +1,51 @@
 package pumping
+
+import "time"
+
+type response struct {
+	Type string `json:"type"`
+	Crs  struct {
+		Type       string `json:"type"`
+		Properties struct {
+			Name string `json:"name"`
+		} `json:"properties"`
+	} `json:"crs"`
+	Features []struct {
+		Type       string `json:"type"`
+		Properties struct {
+			StationID   string    `json:"station_id"`
+			StationName string    `json:"station_name"`
+			FuelID      string    `json:"fuel_id"`
+			Price       string    `json:"price"`
+			CountryID   int       `json:"country_id"`
+			Datetime    time.Time `json:"datetime"`
+		} `json:"properties,omitempty"`
+		Geometry struct {
+			Type        string    `json:"type"`
+			Coordinates []float64 `json:"coordinates"`
+		} `json:"geometry"`
+	} `json:"features"`
+}
+
+type Station struct {
+	ID            string
+	Name          string
+	Brand         string
+	Address       string
+	DieselPrice   string
+	PetrolPrice   string
+	ElectricPrice string
+}
+
+type Features []Feature
+
+type Feature struct{}
+
+type Property struct {
+	StationID   string `json:"station_id"`
+	StationName string `json:"station_name"`
+	FuelID      string `json:"fuel_id"`
+	Price       string `json:"price"`
+	CountryID   string `json:"country_id"`
+	Datetime    string `json:"datetime"`
+}
